efa2a5a4ad96fe3fe632bc10e1c979decfff9ab7
Rohiit Priyadarshan <reachthespace@gmail.com>
2016-12-04 23:32:05 +0530

    mario2
    this is the right solution to the full pyramid

--- mario2.c
+++ mario2.c
@@ -1,34 +1,42 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-
+#include<stdio.h>
+#include<cs50.h>
+int main (void)
 {
     int n;
     int i;
     int j;
-     do{
-        printf("enter the height of the half pyramid(between the range 0 to 23): \n");
-        n = get_int();
-        for ( i=0;i<n+1;i++)
+    do
+    {
+        printf("enter a value for height ranging between 0 to 23:");
+        n=get_int();
+    }while (n<0 || n>23);
+    for(i=0;i<n+1;i++)
+    {
+        for(j=0;j<=n-1;j++)
         {
-            for (j=0; j<n+1;j++)
+            if (j>=n-i)
             {
-                if(j>=n-i){
-                    printf("*");
-                    }else
-                    {
-                        printf(" ");
-                        
-                    }
+                printf("#");
             }
-            if(j=n+1;j=n+2){
-            printf (" ");}printf("\n");
-            for (j=n+3;j<n+n+3;j++)
-            {if (j<=n+3+i){
-                printf("*");
+            else
+            {
+                printf(" ");
             }
         }
+        for(j=n+1;j<n+3;j++)
+        {
+            printf(" ");
         }
-        }while (n<0 || n>23);
+        for(j=n+3;j<n+n+3;j++)
+        {
+            if (j<n+3+i)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }printf("\n");
+    }
 }
